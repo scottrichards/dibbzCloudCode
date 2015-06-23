b0b5701159605b32658544d2e04cec9e4a6a9797
Scott Richards <scott.srichards@gmail.com>
2015-06-22 17:28:40 -0700

    Working on adding all thumbnails

--- cloud/main.js
+++ cloud/main.js
@@ -118,55 +118,69 @@ Parse.Cloud.beforeSave(Parse.User, function(request, response) {
   });
 });
 
-// Parse.Cloud.afterSave("Items", function(request, response) {
-// 
-// 
-// 	Parse.Cloud.httpRequest({
-// 		url: request.object.get("Image").url(),
-// 		success: function(response) {
-// 			// The file contents are in response.buffer.
-// 			var image = new Image();
-// 			return image.setData(response.buffer, {
-// 				success: function() {
-// 					console.log("Image is " + image.width() + "x" + image.height() + ".");
-// 					image.scale({
-// 						width: 64,
-// 						height: 64,
-// 						success: function(image) {
-// 							console.log("Thumbnail image is: " + image.width() + "x" + image.height());
-// 							request.object.set("ImageThumbnail",image);
-// 							request.object.save(null, {
-// 								success: function(dibbzItem) {
-// 									// Execute any logic that should take place after the object is saved.
-// 									console.log('Added thumbnail: ' + dibbzItem.id);
-// 								},
-// 								error: function(dibbzItem, error) {
-// 									// Execute any logic that should take place if the save fails.
-// 									// error is a Parse.Error with an error code and message.
-// 									alert('Failed to update object, with error code: ' + error.message);
-// 								}
-// 							});
-// //							response.success();
-// 						},
-// 						error: function(error) {
-// 							console.log("could not create an image thumbnail");
-// //							response.error("could not create an image thumbnail");
-// 						}
-// 					});
-// 				},
-// 				error: function(error) {
-// 					console.log("The image data was invalid."); 
-// 					response.error("The image data was invalid");
-// 				}
-// 			})
-// 		},
-// 		error: function(error) {
-// 			console.log("The networking request failed");
-// 			response.error("The networking request failed");
-// 			// The networking request failed.
-// 		}
-// 	});
-// });
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("addItemThumbnails", function(request, response) {
+	var Items = Parse.Object.extend("Items");
+	var query = new Parse.Query(Items);
+ 	query.find({
+		success: function(results) {
+			console.log("Successfully retrieved " + results.length + " items.");
+			// Do something with the returned Parse.Object values
+			for (var i = 0; i < 4; i++) {
+				var object = results[i];
+				if (object.get('Thumbnail')) {
+					console.log(object.get('Name') + ' Thumbnail EXISTS ');
+				} else {
+					console.log(object.get('Name') + ' NO Thumbnail ');
+					console.log("url: " + object.get("Image").url());
+					Parse.Cloud.httpRequest({
+							url: object.get("Image").url()
+						}).then(function(response) {
+						  console.log("Loaded Image");
+							var image = new Image();
+							return image.setData(response.buffer);
+						}).then(function(image) {
+							// Resize the image to 64x64.
+							console.log("creating thumbnail at 64x64");
+							return image.scale({
+								width: 64,
+								height: 64
+							});
+
+						}).then(function(image) {
+							// Make sure it's a JPEG to save disk space and bandwidth.
+							console.log("created thumbnail dimensions: " + image.width() + "x" + image.height());
+							return image.setFormat("JPEG");
+
+						}).then(function(image) {
+							// Get the image data in a Buffer.
+							return image.data();
+
+						}).then(function(buffer) {
+							// Save the image into a new file.
+							var base64 = buffer.toString("base64");
+							var cropped = new Parse.File("thumbnail.jpg", { base64: base64 });
+							return cropped.save();
+
+						}).then(function(cropped) {
+							// Attach the image file to the original object.
+							console.log("setting Thumbnail");
+							object.set("Thumbnail", cropped);
+							object.save();
+						}).then(function(result) {
+							response.success();
+						}, function(error) {
+							response.error(error);
+						});
+				}
+			}
+		},
+		error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		}
+	});
+});
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
@@ -207,6 +221,4 @@ Parse.Cloud.define("postMessage", function(request, response) {
 
 	}
 	conversation.set("message", request.params.message);
-
-	
 });
